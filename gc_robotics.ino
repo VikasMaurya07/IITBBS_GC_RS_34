7a4512cc2470fce77c76255973b5bdb305d732ae
ShambaBanerjee <129081469+ShambaBanerjee@users.noreply.github.com>
2023-03-28 20:10:46 +0530

    Update gc_robotics.ino

--- gc_robotics.ino
+++ gc_robotics.ino
@@ -1,8 +1,11 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_PCD8544.h>
-Adafruit_PCD8544 display=Adafruit_PCD8544(8,9,10,11,12);
-TileMap gameTilemap;
+#include <Adafruit_ST7735.h>
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+#define TFT_CS 39
+#define TFT_RST 37
+#define TFT_DC 38
+#define TFT_BACKLIGHT 7
 #define still_player 0;
 #define move_player 1;
 #define jump_player 2;
@@ -14,6 +17,7 @@ TileMap gameTilemap;
 #define Game_play 104;
 #define Game_over 105;
 float hspd=0;
+float hspd_speed=3;
 unsigned int gamestate=Game_intro;
 long Timer_prev=0;
 unsigned long Timer_current=0;
@@ -22,19 +26,26 @@ int player_direction=1;
 int last_direction=1;
 long player_timer=0;
 long player_timer_prev=0;
-
+float sign(float x){
+  if(x>0)
+  return 1;
+  else if(x<0)
+  return -1;
+return 0;
+  
+}
 //intro
 void sceneIntro(){
-  display.clearDisplay();
+  tft.clearDisplay();
   float sizex=13*6;
-  display.setTextSize(1);
-  display.setTextColor(BLACK);
-  display.setCursor((display.width()-sizex)/2,display.height()/2-12);
-  display.display();
+  tft.setTextSize(1);
+  tft.setTextColor(BLACK);
+  tft.setCursor((display.width()-sizex)/2,display.height()/2-12);
+  tft.display();
   Timer_current=mills();
   if (Timer_current-Timer_prev>2500){
     Timer_prev=Timer_current;
-    display.clearDisplay();
+    tft.clearDisplay();
     gamestate=Game_tittle;
   }
 }
@@ -50,9 +61,22 @@ void playerLogic(boolean move_esq,boolean move_dir,boolean jump ){
       player_direction=-1;
       moving=true;
     }
+    if(moving)
+    hspd=hspd_speed*(float)player_direction;
+    
+
   }
 
 }
+void loop(){
+  switch(game_state){
+    case Game_intro: {sceneIntro(); break:}
+  }
+
+}
+
+
+
 
 
 
