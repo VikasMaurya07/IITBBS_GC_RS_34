52d3a685e1c231ca331488429997a5dc30810fd2
Vikas Maurya <123553333+VikasMaurya07@users.noreply.github.com>
2023-03-29 23:40:06 +0530

    Update sensorcode.ino

--- sensorcode/sensorcode.ino
+++ sensorcode/sensorcode.ino
@@ -1,30 +1,26 @@
 #include <Wire.h>
-
-#include <Adafruit_Sensor.h> 
-
+#include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-void setup(void) 
-
-{
-  
+void setup() {
+  Serial.begin(9600);
+  if(!accel.begin()) {
+    Serial.println("Could not find a valid ADXL345 sensor!");
+    while(1);
+  }
 }
 
-void loop(void) 
-
-{
-
-   sensors_event_t event; 
-
-   accel.getEvent(&event);
-
-   Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-
-   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-
-   Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");
+void loop() {
+  sensors_event_t event;
+  accel.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.acceleration.x);
+  Serial.print(" Y: ");
+  Serial.print(event.acceleration.y);
+  Serial.print(" Z: ");
+  Serial.println(event.acceleration.z);
 
    delay(100);
 
